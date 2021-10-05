8cf2d344a9201d8ef27ef8fcda0a93f48475ccdc
Johan Modin <johan.ahlkvist@gmail.com>
2021-10-05 22:27:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ mkdir -p data/input
 while true; do
     read -p "Do you wish to download the default traffic video? (y/n): " yn
     case $yn in
-        [Yy]* ) curl -o data/input/sherbrooke_video.avi https://www.jpjodoin.com/urbantracker/dataset/sherbrooke/sherbrooke_video.avi; break;;
-        [Nn]* ) echo "Not downloading video dataset"; exit;;
+[Yy]* ) curl -O data/input https://www.jpjodoin.com/urbantracker/dataset/sherbrooke/sherbrooke_video.avi -o data/input/sherbrooke_video.avi; break;;
+[Nn]* ) echo "Not downloading video dataset"; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
