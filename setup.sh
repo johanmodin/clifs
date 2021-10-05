7420e0e3b6f0ae00e6193e20cb2d4a4ca154257e
Johan Ahlqvist <johan_ahlkvist@hotmail.com>
2021-10-05 22:29:13 +0200

    cleanup incorrect curl command

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ mkdir -p data/input
 while true; do
     read -p "Do you wish to download the default traffic video? (y/n): " yn
     case $yn in
-[Yy]* ) curl -O data/input https://www.jpjodoin.com/urbantracker/dataset/sherbrooke/sherbrooke_video.avi -o data/input/sherbrooke_video.avi; break;;
+[Yy]* ) curl https://www.jpjodoin.com/urbantracker/dataset/sherbrooke/sherbrooke_video.avi -o data/input/sherbrooke_video.avi; break;;
 [Nn]* ) echo "Not downloading video dataset"; exit;;
         * ) echo "Please answer yes or no.";;
     esac
